8ddea6581d4ad91c4427c1297724d0244d6fcca5
Navya Mallidi <mallidinavya635@gmail.com>
2024-05-31 12:53:19 +0530

    added language

--- data/index.ts
+++ data/index.ts
@@ -50,7 +50,7 @@ export const navItems = [
     {
       id: 5,
       title: "My Tech Stack includes",
-      description: "HTML CSS JavaScript Tailwind React js Next js Python Java C Nodejs Expressjs django soketio mongodb SQL",
+      description: "HTML CSS JavaScript Tailwind React js Next js Python Java C Nodejs Expressjs django soketio mongodb SQL PHP",
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
       titleClassName: "justify-center md:justify-start lg:justify-center",
